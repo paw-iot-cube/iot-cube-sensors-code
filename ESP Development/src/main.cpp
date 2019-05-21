cdc2f92a266658a6dc6a6af26c11de06122740ba
wdubbert <william.dubbert@gmail.com>
2019-05-21 17:01:34 +0200

    Grundgerüst

--- ESP Development/src/main.cpp
+++ ESP Development/src/main.cpp
@@ -3,44 +3,38 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
-#include <WiFiCredentials.h>
 #include <PubSubClient.h>
+
+#include "WiFiCredentials.h"
+#include "readSensorValues.h"
+
+#define MQTT_BROKER_IP "192.168.0.180"
 
 //pins
 #define PIN_D3 0
 #define PIN_D6 12
+#define FIRST_ACTUATOR_ADDRESS 20
+#define HCSR_501 2
+#define BME_280 10
 
-#define PIN_MOTION_IN PIN_D6
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define PIN_ONE_WIRE D6
+
+
+
+bool isDeviceSensor;
+int deviceType = 0;
+
 Adafruit_BME280 bme;
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
 
-void connectToWifi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWD);
-  Serial.print("WiFi Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.printf(".");
-  }
-  Serial.println();
-  Serial.printf("Connection to WiFi network %s established, IP-address: ", WIFI_SSID);
-  Serial.println(WiFi.localIP());
-}
-
-void connectToMosquittoBroker() {
-  mqttClient.setServer("192.168.0.180", 1883);
-  Serial.printf("MQTT Connecting");
-  for (int i = 0; i < 10; i++) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  mqttClient.connect("ESP8266Client");
-  Serial.println("Connected (hopefully)");
-}
+// prototypes
+void connectToWifi();
+void connectToMosquittoBroker(const char* brokerIP);
+int readDeviceType();
+void subscribeReceive(char* topic, byte* payload, unsigned int length);
 
 /*
 char* getLocalIPString() {
@@ -54,34 +48,67 @@ void setup() {
   // open serial interface for monitoring purposes
   Serial.begin(115200);
   Serial.println();
-  // open I2C connection to BME @ address 0x76
-  bme.begin(0x76);
-  // configure motion sensor read pin
-  pinMode(PIN_MOTION_IN, INPUT);
+
+  pinMode(D0, INPUT);
+  pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
+  pinMode(D7, INPUT);
+  pinMode(D8, INPUT);
+
+  int deviceType = readDeviceType();
+
+  switch (deviceType) {
+    case BME_280:
+      bme.begin(0x76);
+      break;
+    case HCSR_501:
+      pinMode(PIN_ONE_WIRE, INPUT);
+      break;
+    default:
+      break;
+  }
+  Serial.printf("device Type %d\n", deviceType);
+
 
   // connect to WiFi Network
   connectToWifi();
-  connectToMosquittoBroker();
+  // connect to mosquitto broker
+  connectToMosquittoBroker(MQTT_BROKER_IP);
 
+  // TO DO
   //char locIP[16] = "";
   //locIP = getLocalIPString();
   //printf("My local IP: %s\n", String(WiFi.localIP()));
 }
 
-// doesn't work at the moment
-void detectMotion() {
-  static bool motiondetectStillActive = false;
-  if(digitalRead(PIN_MOTION_IN) && !motiondetectStillActive) {
-    Serial.printf("MOTION DETECTED!\n");
-    mqttClient.publish("sensors/1/motion", "MOTION DETECTED");
-    motiondetectStillActive = true;
-  } else if (digitalRead(PIN_MOTION_IN)) {
-    motiondetectStillActive = false;
-  }
-}
-
 void loop() {
-  static int count = 0;
+  mqttClient.loop();
+  mqttClient.subscribe("topic");
+  if (isDeviceSensor) {
+    switch (deviceType) {
+      case HCSR_501:
+        readHCSR501(mqttClient, PIN_ONE_WIRE, "sensors/1/motion");
+        break;
+      case BME_280:
+        readBME208(mqttClient, bme, "sensors/1/temp", "test", "test");
+        break;
+
+      default:
+        break;
+    }
+  } else {
+    switch (deviceType) {
+      case 20:
+        break;
+      case 21:
+        break;
+      default:
+        break;
+    }
+  }
+
+
+  /*static int count = 0;
   if (count >= 5) {
     Serial.println("sending temperature to RPi");
     char temperature[10] = "";
@@ -91,10 +118,68 @@ void loop() {
   }
   count++;
 
-  Serial.printf("Temperature: %.1f°C\n", bme.readTemperature());
-  Serial.printf("Altitude: %.0fm\n", bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.printf("Pressure: %.0fhPa\n", bme.readPressure() / 100.0F);
-  detectMotion();
+
+  readHCSR501();
   Serial.println("");
+  delay(1000);*/
   delay(1000);
+}
+
+void connectToWifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+  Serial.print("WiFi Connecting");
+  // sometimes stuck in while-loop; TO DO
+  /*
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.printf(".");
+  }
+  */
+  // simulating waiting for connection, hopefully established after 5s
+  for (int i = 0; i < 10; i++) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.printf("Connection to WiFi network %s established, IP-address: ", WIFI_SSID);
+  Serial.println(WiFi.localIP());
+}
+
+void connectToMosquittoBroker(const char* brokerIP) {
+  mqttClient.setServer(brokerIP, 1883);
+  Serial.printf("MQTT Connecting");
+  for (int i = 0; i < 10; i++) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  mqttClient.connect("ESP8266Client");
+  Serial.println("Connected (hopefully)");
+  mqttClient.setCallback(subscribeReceive);
+}
+
+int readDeviceType()
+{
+  int deviceTypePins[5]= {D0,D5,D6,D7,D8};
+
+  for (int i = 0; i < 5; i++) {
+    if (digitalRead(deviceTypePins[i])) {
+      deviceType+=pow(2,i);
+    }
+  }
+
+  isDeviceSensor = (deviceType < FIRST_ACTUATOR_ADDRESS);
+
+  return deviceType;
+}
+
+void subscribeReceive(char* topic, byte* payload, unsigned int length) {
+  Serial.println("MQTT MESSAGE RECEIVED");
+  Serial.printf("Topic: %s\n");
+  Serial.print("Message: ");
+  for(int i = 0; i < length; i ++)
+  {
+    Serial.print(char(payload[i]));
+  }
+  Serial.println();
 }
---
+++ ESP Development/src/readSensorValues.cpp
@@ -0,0 +1,57 @@
+#include "readSensorValues.h"
+#include "debug.h"
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+void readHCSR501(PubSubClient mqtt, int pin, const char* topic) {
+  static bool motionDetected;
+  motionDetected = digitalRead(pin);
+  mqtt.publish(topic, (motionDetected)?"true":"false");
+  #ifdef DEBUG
+    Serial.printf("motion detected: %d\n", motionDetected);
+  #endif
+  /*
+  static bool motiondetectStillActive = false;
+  if(digitalRead(pin) && !motiondetectStillActive) {
+    Serial.printf("MOTION DETECTED!\n");
+    //mqttClient.publish("sensors/1/motion", "MOTION DETECTED");
+    motiondetectStillActive = true;
+  } else if (digitalRead(pin)) {
+    motiondetectStillActive = false;
+  }
+  */
+}
+
+void readBME208(PubSubClient mqtt, Adafruit_BME280 bme, const char* tempTopic, const char* pressTopic, const char* humidTopic) {
+  static float temperature, pressure, humidity;
+  temperature = bme.readTemperature();
+  pressure = bme.readPressure();
+  humidity = bme.readHumidity();
+
+  char tempString[7] = "";
+  sprintf(tempString, "%.2f", temperature);
+  mqtt.publish(tempTopic, tempString);
+  #ifdef DEBUG
+    Serial.printf("Temperature: %.1f°C\n", temperature);
+  #endif
+
+  char pressString[7] = "";
+  sprintf(pressString, "%.0f", pressure);
+  mqtt.publish(pressTopic, pressString);
+  #ifdef DEBUG
+    Serial.printf("Pressure: %.2fhPa\n", pressure / 100.0F);
+  #endif
+
+  char humidString[7] = "";
+  sprintf(humidString, "%.2f", humidity);
+  mqtt.publish(humidTopic, humidString);
+  #ifdef DEBUG
+    Serial.printf("Humidity: %.2f%%\n", humidity);
+  #endif
+
+  /*
+  Serial.printf("Temperature: %.1f°C\n", bme.readTemperature());
+  Serial.printf("Altitude: %.0fm\n", bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.printf("Pressure: %.0fhPa\n", bme.readPressure() / 100.0F);
+  */
+}
