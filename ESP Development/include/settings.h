8b2fc82e289e2a5582fc54295253ec7a2a5e3dc4
simdick <49993966+simdick@users.noreply.github.com>
2019-06-04 19:02:01 +0200

    hopefully fixed

--- ESP Development/include/settings.h
+++ ESP Development/include/settings.h
@@ -2,10 +2,10 @@
 #define SETTINGS_H
 
 // uncomment to enable debug messages
-#define DEBUG
+//#define DEBUG
 
 // mqtt-broker IP
-#define MQTT_BROKER_IP "192.168.188.22"
+#define MQTT_BROKER_IP "192.168.0.180"
 // TO DO: find another way of configuring this
 
 // standard read intervals for different sensors in ms
@@ -13,8 +13,8 @@
 #define STD_INTERVAL_MS_HCSR501 10000
 
 // wifi credentials
-#define WIFI_SSID "FRITZ!Box 7490"
-#define WIFI_PASSWD "26489093048613568547"
+#define WIFI_SSID "TPLINK_PAW"
+#define WIFI_PASSWD "48414647"
 // TO DO: find another way of configuring and storing these
 
 #endif
